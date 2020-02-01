dc0531c052fcd3a47102a704c8198e58c951be8e
Aaquib Niaz <aaquibniaz3600@gmail.com>
2020-02-02 01:24:31 +0530

    added styling

--- account/models.py
+++ account/models.py
@@ -3,9 +3,9 @@ from django.contrib.auth.models import  User
 
 # Create your models here.
 class UserProfile(models.Model):
-    user        =   models.OneToOneField(User,on_delete=models.CASCADE)
-    location    =   models.CharField(max_length=30,null=True, blank=True)
-    address     =   models.CharField(max_length=255,null=True, blank=True)
+    user        = models.OneToOneField(User,on_delete=models.CASCADE)
+    location    = models.CharField(max_length=30,null=True, blank=True)
+    address     = models.CharField(max_length=255,null=True, blank=True)
 
     def __str__(self):
         return  self.user.username
